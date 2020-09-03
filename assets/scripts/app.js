ceaa66973393308f5f197afc77b234178cbb5259
Carolina <daza.jeymmy@gmail.com>
2020-09-03 14:24:11 -0500

    Archivos de desarrollo de la calculadora

---
+++ assets/scripts/app.js
@@ -0,0 +1,65 @@
+const historial = [];
+let defaultResult = 0;
+let currentResult = defaultResult;
+let numeroMarcado = 0;
+
+
+function suma()
+{
+    calcular('suma');
+}
+addBtn.addEventListener('click',suma);
+
+
+function resta()
+{
+    calcular('resta');
+}
+subtractBtn.addEventListener('click',resta);
+
+
+function multiplicacion()
+{
+    calcular('multiplicacion');
+}
+multiplyBtn.addEventListener('click',multiplicacion); 
+
+
+function division()
+{
+    calcular('division');
+}
+divideBtn.addEventListener('click',division); 
+
+
+function arregloHistorial()
+{
+    alert(historial);
+}
+historialBtn.addEventListener('click',arregloHistorial);
+
+function calcular(operador)
+{
+    if(operador === 'suma')
+    {
+        currentResult =  currentResult + parseInt(userInput.value);
+    }else if(operador === 'resta')
+    {        
+        currentResult = currentResult - parseInt(userInput.value);
+
+    }else if (operador === 'multiplicacion')
+    {
+        currentResult = currentResult * parseInt(userInput.value); 
+
+    }else if(operador === 'division')
+    {
+        currentResult = currentResult / parseInt(userInput.value);
+    }
+    outputResult(currentResult, currentResult);
+    userInput.value = '';
+}
+
+
+
+
+
---
+++ assets/scripts/vendor.js
@@ -0,0 +1,41 @@
+const userInput = document.getElementById('input-number');
+const addBtn = document.getElementById('btn-add');
+const subtractBtn = document.getElementById('btn-subtract');
+const multiplyBtn = document.getElementById('btn-multiply');
+const divideBtn = document.getElementById('btn-divide');
+const historialBtn = document.getElementById('btn-historial');
+const numeroNueve = document.getElementById('btn-nueve');
+const numeroOcho = document.getElementById('btn-ocho');
+const numeroSiete = document.getElementById('btn-siete');
+const numeroSeis = document.getElementById('btn-seis');
+const numeroCinco = document.getElementById('btn-cinco');
+const numeroCuatro = document.getElementById('btn-cuatro');
+const numeroTres = document.getElementById('btn-tres');
+const nuemroDos = document.getElementById('btn-dos');
+const numeroUno = document.getElementById('btn-uno');
+const numeroZero = document.getElementById('btn-cero');
+const resetc = document.getElementById('btn-c');
+const igual = document.getElementById('btn-igual');
+const numero;
+
+
+
+numeroNueve.onclick = function(e)
+{
+    userInput.textContent = userInput.textContent + "9";
+    alert(userInput.value);
+}
+
+ 
+
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
+
+function outputResult(result, text) {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
+}
+
+// Eventos
+
+
