fb9b1bd88b637bab254a8bf04922d3fc523e7a3d
Carolina <daza.jeymmy@gmail.com>
2020-09-11 17:51:10 -0500

    Se agrego codigo para eliminar y crear nodos

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 
 //innerText me trae el texto que esta entre el boton; Array.prototype.forEach.call(teclas, (tecla) toma la lista y la deja navegar
+let esCientifica = false;
 for (const tecla of teclas) 
 {
     tecla.onclick = () => 
@@ -41,3 +42,36 @@ function cambiarColor(){
     }
     
 }
+// codigo para agregar nodos en este caso se agrego un array de botones
+function calculadoraCientifica()
+{
+    if( esCientifica === false)
+    { 
+        let botones = ['Pi','i','e'];
+        let elemento = document.getElementById('calc-actions1');
+        for (let i = 0; i < botones.length; i++) {
+            let boton = document.createElement('button');
+            boton.innerText = botones[i];
+            elemento.appendChild(boton);
+            
+        }       
+        esCientifica = true;
+    }else
+    {
+        location.reload();
+    } 
+    
+}
+calculadoraCient.addEventListener('click',calculadoraCientifica);
+
+
+// codigo para eliminar nodos en este caso se elimino un <P id="parrafo">
+// La propiedad parentNode es para acceder al padre
+function eliminarNodos()
+{
+    let texto = document.getElementById('parrafo');
+    texto.parentNode.removeChild(texto);
+
+}
+eliminarNodo.onclick = () => eliminarNodos();
+
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -3,6 +3,8 @@ const historialBtn = document.getElementById('btn-historial');
 const teclas = document.getElementsByClassName('tecla');
 const resetc = document.getElementById('btn-c');
 const igual = document.getElementById('btn-igual');
+const calculadoraCient = document.getElementById('btn-cientifica');
+const eliminarNodo = document.getElementById('btn-eliminar');
 const historial = [];
 
 
